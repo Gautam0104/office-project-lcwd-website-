cd17aceb29b409fb091b35272c2373724a1e338e
Gautam0104 <111377889+Gautam0104@users.noreply.github.com>
2023-02-16 23:04:25 +0530

    blogs-commit

--- components/Home.js
+++ components/Home.js
@@ -1,38 +1,13 @@
 import React from 'react'
-import HeroSection from './HeroSection'
-import Motivation from './Motivation'
 
-import Video from './Video'
 
 
 const Home = () => {
   
   return (
-    <div>
-   
-
-   <HeroSection />
-   
-
-    <Motivation />
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-  <Video />
-
-
-    </div>
+<div>
+  
+</div>
 
 
    
